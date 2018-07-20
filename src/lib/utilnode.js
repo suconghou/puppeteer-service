f36ab1abbba7deec38f1cd36d5bdb853989e9fad
suconghou <1126045770@qq.com>
2018-07-20 10:00:27 +0800

    hide info

--- src/lib/utilnode.js
+++ src/lib/utilnode.js
@@ -1,5 +1,4 @@
 import process from 'process';
-import os from 'os';
 import { Buffer } from 'buffer';
 
 export default {
@@ -8,12 +7,7 @@ export default {
 			pid: process.pid,
 			node: process.version,
 			os: process.platform + process.arch,
-			freemem: Math.round(os.freemem() / 1048576),
-			allmem: Math.round(os.totalmem() / 1048576),
-			cpus: os.cpus(),
-			load: os.loadavg(),
-			uptime: process.uptime(),
-			memory: process.memoryUsage()
+			uptime: process.uptime()
 		};
 		return data;
 	},
